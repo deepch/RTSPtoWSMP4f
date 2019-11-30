655a55b146eacf6fca1d47262c7331ea97acf650
Andrey Semochkin <deepweb@MacBook-Air-Andrey.local>
2019-11-30 23:06:45 +0100

    First commit

---
+++ http.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sort"
+	"time"
+
+	"github.com/deepch/vdk/format/mp4f"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/net/websocket"
+)
+
+func serveHTTP() {
+	router := gin.Default()
+	router.LoadHTMLGlob("web/templates/*")
+	router.GET("/", func(c *gin.Context) {
+		fi, all := Config.list()
+		sort.Strings(all)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"port":     Config.Server.HTTPPort,
+			"suuid":    fi,
+			"suuidMap": all,
+		})
+	})
+	router.GET("/player/:suuid", func(c *gin.Context) {
+		_, all := Config.list()
+		sort.Strings(all)
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"port":     Config.Server.HTTPPort,
+			"suuid":    c.Param("suuid"),
+			"suuidMap": all,
+		})
+	})
+	router.GET("/ws/:suuid", func(c *gin.Context) {
+		handler := websocket.Handler(ws)
+		handler.ServeHTTP(c.Writer, c.Request)
+	})
+	router.StaticFS("/static", http.Dir("web/static"))
+	err := router.Run(Config.Server.HTTPPort)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func ws(ws *websocket.Conn) {
+	defer ws.Close()
+	suuid := ws.Request().FormValue("suuid")
+	log.Println("Request", suuid)
+	if Config.ext(suuid) {
+		ws.SetWriteDeadline(time.Now().Add(5 * time.Second))
+		suuid := ws.Request().FormValue("suuid")
+		cuuid, ch := Config.clAd(suuid)
+		defer Config.clDe(cuuid)
+		codecs := Config.coGe(suuid)
+		if codecs == nil {
+			log.Println("No Codec Info")
+			return
+		}
+		muxer := mp4f.NewMuxer(nil)
+		muxer.WriteHeader(codecs)
+		meta, init := muxer.GetInit(codecs)
+		err := websocket.Message.Send(ws, append([]byte{9}, meta...))
+		if err != nil {
+			return
+		}
+		err = websocket.Message.Send(ws, init)
+		if err != nil {
+			return
+		}
+		var start bool
+		for {
+			select {
+			case pck := <-ch:
+				if pck.IsKeyFrame {
+					start = true
+				}
+				if !start {
+					continue
+				}
+				ready, buf, _ := muxer.WritePacket(pck, false)
+				if ready {
+					ws.SetWriteDeadline(time.Now().Add(10 * time.Second))
+					err := websocket.Message.Send(ws, buf)
+					if err != nil {
+						return
+					}
+				}
+			}
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	go serveHTTP()
+	go serveStreams()
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigs
+		log.Println(sig)
+		done <- true
+	}()
+	log.Println("Server Start Awaiting Signal")
+	<-done
+	log.Println("Exiting")
+}
